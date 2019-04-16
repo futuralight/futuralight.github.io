86b7d56891e84cd9976bf3f9f318c3cca8109012
futuralight <49651372+futuralight@users.noreply.github.com>
2019-04-16 17:03:18 +0500

    Add files via upload

---
+++ scripts/skripts.js
@@ -0,0 +1,54 @@
+
+
+
+function doalert(checkboxElem) 
+{
+
+  var card = checkboxElem.parentNode;
+var child = null;
+
+for (var i = 0; i < card.childNodes.length; i++) {
+    if (card.childNodes[i].className == "c2"||card.childNodes[i].className == "c1") {
+      child = card.childNodes[i];
+      break;
+    }    }
+
+if(child.className=="c2")
+{
+  child.className="c1";
+}
+else{
+
+    child.className="c2";
+}
+
+
+
+
+}
+
+function doalertb(buttn) 
+{
+
+  var card = buttn.parentNode;
+var child = null;
+
+for (var i = 0; i < card.childNodes.length; i++) {
+    if (card.childNodes[i].className == "c2"||card.childNodes[i].className == "c1") {
+      child = card.childNodes[i];
+      break;
+    }    }
+
+if(child.className=="c2")
+{
+  child.className="c1";
+}
+else{
+
+    child.className="c2";
+}
+
+
+
+
+}
